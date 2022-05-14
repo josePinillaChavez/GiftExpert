85c1a9b71f25b7aa7f928655774fbe758d3d8088
Jose Pinilla <josepcinf@gmail.com>
2022-05-14 13:13:13 -0400

    Docs Agregados

---
+++ docs/precache-manifest.3601fe2c69d2943e46f242558ff0481c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ae49d67e93119efd7ea7c9a4a931c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1778ea9de2c70018fb49",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1778ea9de2c70018fb49",
+    "url": "/static/js/main.83b83414.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
